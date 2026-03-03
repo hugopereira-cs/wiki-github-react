ea709d994dfb71856aefe5bb5c7d4cb4733cdeaa
hugopereira-cs <hugoper_cs@yahoo.com>
2026-03-03 12:38:52 -0300

    refactor: Button styles

--- src/components/Button/Button.styles.js
+++ src/components/Button/Button.styles.js
@@ -1,21 +1,54 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const variants = {
+  primary: css`
+    background-color: #fafafa;
+    border: 1px solid #fafafa;
+    color: #22272e;
+
+    &:hover {
+      background-color: #fafafa40;
+      color: #fff;
+    }
+  `,
+  danger: css`
+    width: 15%;
+    min-width: 150px;
+    background-color: transparent;
+    color: #ff6b6b;
+    border: 1px solid #ff6b6b;
+    margin: 20px 0;
+
+    &:hover {
+      background-color: #ff6b6b40;
+      color: #fff;
+    }
+  `
+};
 
 export const ButtonContainer = styled.button`
-  background-color: #fafafa;
-  border: 1px solid #fafafa;
   border-radius: 20px;
-
   height: 62px;
   width: 80%;
   font-size: 20px;
-
   margin: 20px;
+  cursor: pointer;
   transition: transform 0.5s ease;
 
   &:hover {
-    background-color: #fafafa40;
-    color: #fff;
-    cursor: pointer;
     transform: scale(1.01);
   }
+
+  &:disabled {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+
+  {/**
+   * Usa a desestruturação para extrair a prop variant.
+   * Busca um valor dentro do objeto "variants".
+   * Se "variants[variant] retornar um valor "falsy" (undefined, por exemplo), a expressão usará o
+   * valor de primary como padrão.
+   */}
+  ${({ variant }) => variants[variant] || variants.primary}
 `;
