aacb60326087cea66f12e0e6e3b8ce1890107b94
hugopereira-cs <hugoper_cs@yahoo.com>
2026-03-03 09:58:16 -0300

    add: props validation

--- src/components/ItemRepo/ItemRepo.jsx
+++ src/components/ItemRepo/ItemRepo.jsx
@@ -1,22 +1,33 @@
+import PropTypes from 'prop-types';
 import { ItemContainer } from './ItemRepo.styles';
 
-export default function ItemRepo({repo, handleRemoveRepo}) {
+export default function ItemRepo({ repo, handleRemoveRepo }) {
   const handleRemove = () => {
     handleRemoveRepo(repo.id);
   };
 
   return (
-    <ItemContainer onClick={handleRemove}>
+    <ItemContainer>
       <h3>{repo.name}</h3>
       <p>{repo.full_name}</p>
       <a href={repo.html_url} rel="noreferrer" target="_blank">
         Ver repositório
       </a>
       <br />
-      <a href="#" rel="noreferrer" className="remover" onClick={handleRemoveRepo}>
+      <a href="#" rel="noreferrer" className="remover" onClick={(e) => { e.preventDefault(); handleRemove(); }}>
         Remover
       </a>
       <hr />
     </ItemContainer>
   );
 }
+
+ItemRepo.propTypes = {
+  repo: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    full_name: PropTypes.string.isRequired,
+    html_url: PropTypes.string.isRequired
+  }).isRequired,
+  handleRemoveRepo: PropTypes.func.isRequired
+};
